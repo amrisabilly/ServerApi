9da446c22c599eb029e93fb98b436eda9d982af1
Devsam_ <sabillyamri1@gmail.com>
2025-08-04 06:27:13 +0700

    add laravel-share

--- app/Http/Controllers/ArtikelController.php
+++ app/Http/Controllers/ArtikelController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Jorenvh\Share\ShareFacade as Share;
+use Illuminate\Support\Str;
 
 class ArtikelController extends Controller
 {
@@ -15,6 +17,31 @@ class ArtikelController extends Controller
     }
 
     public function show($id)
+    {
+        $article = $this->getArticleById($id);
+
+        if (!$article) {
+            abort(404);
+        }
+
+        // Custom share URLs dengan gambar dan judul
+        $currentUrl = request()->fullUrl();
+        $imageUrl = asset('artikel/' . $article['image']);
+        $title = $article['title'];
+        $description = Str::limit($article['content'], 100);
+
+        $shareButtons = [
+            'facebook' => "https://www.facebook.com/sharer/sharer.php?u=" . urlencode($currentUrl) . "&t=" . urlencode($title),
+            'twitter' => "https://twitter.com/intent/tweet?url=" . urlencode($currentUrl) . "&text=" . urlencode($title . " - " . $description),
+            'linkedin' => "https://www.linkedin.com/sharing/share-offsite/?url=" . urlencode($currentUrl),
+            'whatsapp' => "https://wa.me/?text=" . urlencode($title . " - " . $currentUrl),
+            'telegram' => "https://t.me/share/url?url=" . urlencode($currentUrl) . "&text=" . urlencode($title)
+        ];
+
+        return view('artikel.detail', compact('article', 'shareButtons'));
+    }
+
+    private function getArticleById($id)
     {
         $jsonPath = public_path('artikel/dataArtikel.json');
         $data = json_decode(file_get_contents($jsonPath), true);
@@ -30,10 +57,6 @@ class ArtikelController extends Controller
             }
         }
 
-        if (!$article) {
-            abort(404);
-        }
-
-        return view('artikel.detail', compact('article'));
+        return $article;
     }
 }
