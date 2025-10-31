c1a87e5131970fe68a04180cc7272c3c79066fbe
mahendrak <bimamahendra0705@gmail.com>
2025-10-31 22:46:06 +0700

    fix: routes pengguna api

--- routes/api.php
+++ routes/api.php
@@ -75,5 +75,6 @@ Route::prefix('bencana')->group(function () {
         Route::post('/login', [PenggunaController::class, 'login']);
     });
     Route::apiResource('pengguna', PenggunaController::class);
-    
+     // Route upload photo
+    Route::post('pengguna/{id}/upload-photo', [PenggunaController::class, 'uploadPhoto']);
 });
