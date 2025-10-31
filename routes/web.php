efd64a67b91d3b3011f4889e0e97a612d4e764b6
Zakinanda Faishal <zakinanda.faishal@gmail.com>
2025-10-31 20:31:12 +0700

    add

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\landing\BerandaController;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\MahasiswaController;
-
+use App\Http\Controllers\Api\MbahOerip\GambarController;
 
 
 Route::Resource('/mahasiswa', MahasiswaController::class);
@@ -28,3 +28,10 @@ Route::get('/article/{id}', [ArticleController::class, 'show'])->name('article.s
 // Tambahkan route untuk komentar
 Route::post('/comments', [CommentController::class, 'store'])->name('comments.store');
 Route::post('/comments/reply', [\App\Http\Controllers\CommentController::class, 'reply'])->name('comments.reply');
+
+
+Route::prefix('mbah-oerip')->group(function () {
+    Route::get('/product/create', [GambarController::class, 'create'])->name('product.create');
+    Route::post('/product/store', [GambarController::class, 'store'])->name('product.store');
+    Route::get('/product/list', [GambarController::class, 'list'])->name('product.list');
+});
