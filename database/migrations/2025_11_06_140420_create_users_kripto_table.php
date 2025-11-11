a5d1b2b156ba8bcfbdf13b30ebe2e4e96a3b2602
Devsam_ <sabillyamri1@gmail.com>
2025-11-11 13:16:48 +0700

    add private key

--- database/migrations/2025_11_06_140420_create_users_kripto_table.php
+++ database/migrations/2025_11_06_140420_create_users_kripto_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('email');
             $table->string('password');
             $table->text('public_key');
+            $table->text('private_key');
             $table->string('profile_photo_url')->nullable();
             $table->text('bio')->nullable();
             $table->rememberToken();
